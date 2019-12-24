0b30b6d4420e7c9fda7a004aea03e5b9282f90fd
vladislav <govoritov2018@gmail.com>
2019-12-24 19:06:56 +0300

    lesson 9.1

--- Lesson 9/js/script.js
+++ Lesson 9/js/script.js
@@ -35,41 +35,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-   /*  let deadline = "2018-11-21"
-
-    function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t / 1000) % 60),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor((t / (1000 * 60 * 60)));
-        return {
-            'total': t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-
-    function setClock(id, endtime) {
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            interval = setInterval(uppdateClock, 1000);
-
-        function uppdateClock() {
-            let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
-
-            if (t.total <= 0) {
-                clearInterval(interval);
-            }
-        }
-    }
-    setClock('timer', deadline);
-}); */
+   
 
 let deadline = '2019-12-25';
 
